d47bf8ba517ec4ed76d5ac6e61048daee4c0fcc7
Mirmahas <morenako361@gmail.com>
2024-07-23 11:36:30 +0200

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,37 +11,17 @@ const Navbar = () => {
 
   return (
     <nav className="bg-blue-500 p-4">
-      <div className="container mx-auto flex items-center justify-between">
+      <div className="container mx-auto flex items-center">
         <Link to="/" className="text-2xl font-bold text-white">
           MyLogo
         </Link>
         <div className="ml-auto flex items-center">
-          <button
-            className="text-white focus:outline-none md:hidden"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              ></path>
-            </svg>
-          </button>
           <div className="relative ml-4">
-            <div className="h-12 w-12 cursor-pointer" onClick={toggleMenu}>
-              <img
-                className="md:ml-4 md:mt-0"
-                src="https://thumbs.dreamstime.com/z/icono-de-men%C3%BA-web-s%C3%ADmbolo-del-hamburguesa-vector-279634421.jpg?w=768"
-                alt="Icono de lista"
-              />
+            <div
+              className="align-content: center, w-12 cursor-pointer"
+              onClick={toggleMenu}
+            >
+              <GiHamburgerMenu />
             </div>
             {menuVisible && (
               <div className="absolute right-0 mt-2 w-48 rounded-lg border border-gray-200 bg-white shadow-lg">
