3b812020a802dadc0a1d49ac9d207b9e16b79275
Mirko Fede <mirko@mirkoeffe.shop>
2024-07-23 15:52:43 +0200

    login form feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Homepage from "./pages/Homepage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import About from "./pages/About";
+import Login from "./pages/Login";
 export default function App() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
         {/* <Route path="/market" element={<Market />} />
         <Route path="/market/:id" element={<MarketDetail />} />
         <Route path="/favourite-list" element={<FavouriteList />} />
