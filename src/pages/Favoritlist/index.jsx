b5945036f51a38d3e92c2ccf55fda512c649a0c0
Mirko Fede <mirko@mirkoeffe.shop>
2024-07-26 17:22:05 +0200

    fix not used import to deploy app

--- src/pages/Favoritlist/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Favoritlist() {
-  return <div></div>;
-}
-
-export default Favoritlist;
--- src/pages/Market/index.jsx
+++ src/pages/Market/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import CryptoSection from "./cryptoSection";
 import ForexSection from "./forexSection";
 import StocksSection from "./stockSection";
