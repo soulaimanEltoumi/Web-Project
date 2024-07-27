9bf3481d4a37576cbc48a348dbcfe5f338692d25
Mirko Fede <mirko@mirkoeffe.shop>
2024-07-28 01:22:23 +0200

    add edit profile feature

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -38,7 +38,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, setUser, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
